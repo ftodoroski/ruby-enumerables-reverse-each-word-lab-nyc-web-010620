3b075d13fe150a455688d5ac60184f1f986db4f4
Filip Todoroski <ftodoroski@ymail.com>
2019-11-24 17:15:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word(sentence)
-  reversed_letters = sentence.split(' ').collect { |word| word.reverse }
-  reversed_letters.join(' ')
-end
+# def reverse_each_word(sentence)
+#   reversed_letters = sentence.split(' ').collect { |word| word.reverse }
+#   reversed_letters.join(' ')
+# end
 
 
 def reverse_each_word(sentence)
