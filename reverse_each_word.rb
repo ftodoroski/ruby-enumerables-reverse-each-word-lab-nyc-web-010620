8a26d32751973bb0f671f11f1f5d1601a5e9e9bd
Filip Todoroski <ftodoroski@ymail.com>
2019-11-24 17:20:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,10 +4,10 @@ def reverse_each_word(sentence)
 end
 
 # Using each
-def reverse_each_word(sentence)
-  words = sentence.split(' ')
-  new_sentence = []
-  words.each { |element| new_sentence << element.reverse }
-
-  new_sentence.join(' ')
-end
+# def reverse_each_word(sentence)
+#   words = sentence.split(' ')
+#   new_sentence = []
+#   words.each { |element| new_sentence << element.reverse }
+#
+#   new_sentence.join(' ')
+# end
